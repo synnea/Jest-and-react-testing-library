fda92abfbdd39b74ab21c402db502ef525359f39
Carina <carrie.poell@gmail.com>
2021-03-24 22:35:15 +0100

    test: code up color button change

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,9 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  //click button
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+  expect(colorButton.textContent).toBe('Change to red');
 });
-test('button turns blue when clicked', () => {});
